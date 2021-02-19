32ce85f1ffe669cd4b8a45a2e9b90704e583536e
jolyne1y <919957513@qq.com>
2021-02-19 17:46:03 +0800

    提交的第一个c代码

---
+++ test,c.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+//int main()
+//{
+//	printf("hello world\n");
+//}
+
+//练习--将1~100之间的奇数打印出来
+int main()
+{
+	int i = 1;
+	while (i <= 100)
+	{
+		printf("%d\n", i);
+		i += 2;
+	}
+	return 0;
+}
